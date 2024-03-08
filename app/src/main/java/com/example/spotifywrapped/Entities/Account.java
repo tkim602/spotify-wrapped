32b1e3bcc733a6f9f884b0c69d4d284000ded4b2
Max Xu <maxxu123456@gmail.com>
2024-03-07 23:08:52 -0500

    Connected database to Main Activity and added very minimal sign up screen in Main Activity and allows developer to see data in database

--- app/src/main/java/com/example/spotifywrapped/Entities/Account.java
+++ app/src/main/java/com/example/spotifywrapped/Entities/Account.java
@@ -1,5 +1,6 @@
 package com.example.spotifywrapped.Entities;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -46,5 +47,10 @@ public class Account {
 
     public void setAccountPassword(String accountPassword) {
         this.accountPassword = accountPassword;
+    }
+    @NonNull
+    @Override
+    public String toString() {
+        return "Account ID: " + accountID + ", Email: " + accountEmail + ", Password: " + accountPassword;
     }
 }

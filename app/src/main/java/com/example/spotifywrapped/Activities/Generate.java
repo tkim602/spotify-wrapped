671c6283284e50ef04196254a0e2478f93107cdc
BuildSoftT <bencordova212@gmail.com>
2024-03-19 18:28:44 -0400

    generate V4

--- app/src/main/java/com/example/spotifywrapped/Activities/Generate.java
+++ app/src/main/java/com/example/spotifywrapped/Activities/Generate.java
@@ -1,26 +1,125 @@
 package com.example.spotifywrapped.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
-import com.example.spotifywrapped.Entities.Account;
 import com.example.spotifywrapped.R;
-import com.example.spotifywrapped.SpotifyWrappedDatabase;
-import com.example.spotifywrapped.SpotifyWrappedViewModel;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+//Full of Errors in this java file; still working on it.
 public class Generate extends AppCompatActivity {
+    private Call mCall;
+    private String mAccessToken, mAccessCode;
+    private final OkHttpClient mOkHttpClient = new OkHttpClient();
+    public static final int AUTH_TOKEN_REQUEST_CODE = 0;
+    public static final int AUTH_CODE_REQUEST_CODE = 1;
+    public static final String CLIENT_ID = "306fb5543abb4f23b00ae1a5d1d70886";
+    public static final String REDIRECT_URI = "spotifywrapped://auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generate);
 
+        /*MainActivity test = new MainActivity();
+        mAccessToken = test.getAccessToken();*/
+
+        //Initialize the buttons
+        Button goBtn = (Button) findViewById(R.id.button_go);
+        //Set click listeners for buttons
+        goBtn.setOnClickListener((v) -> {
+            onGoButtonClicked();
+        });
+    }
+
+    public void onGoButtonClicked() {
+
+        if (mAccessToken == null) {
+            Toast.makeText(this, "You need to get an access token first!", Toast.LENGTH_SHORT).show();
+            final AuthorizationRequest request3 = getAuthenticationRequest(AuthorizationResponse.Type.TOKEN);
+            AuthorizationClient.openLoginActivity(Generate.this, AUTH_TOKEN_REQUEST_CODE, request3);
+            return;
+        }
+
+        final Request request = new Request.Builder()
+                .url("https://api.spotify.com/v1/me/top/artists")
+                .addHeader("Authorization", "Bearer " + mAccessToken)
+                .build();
+        if (mCall != null) {
+            mCall.cancel();
+        }
+        mCall = mOkHttpClient.newCall(request);
+        mCall.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("HTTP", "Failed to fetch data: " + e);
+                Toast.makeText(Generate.this, "Failed to fetch data, watch Logcat for more details",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+                try {
+                    final JSONObject jsonObject = new JSONObject(response.body().string());
+                    JSONArray jsonArray = jsonObject.optJSONArray("items");
+                    runOnUiThread(() -> ((TextView) findViewById(R.id.generateText)).setText(jsonArray.optJSONObject(0).optString("name")));
+                } catch (JSONException e) {
+                    Log.d("JSON", "Failed to parse data: " + e);
+                    Toast.makeText(Generate.this, "Failed to parse data, watch Logcat for more details",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final AuthorizationResponse response = AuthorizationClient.getResponse(resultCode, data);
+
+        // Check which request code is present (if any)
+        if (AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            mAccessToken = response.getAccessToken();
+            Log.d("Access Token", mAccessToken);
+
+        }
+    }
+
+    private AuthorizationRequest getAuthenticationRequest(AuthorizationResponse.Type type) {
+        return new AuthorizationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
+                .setShowDialog(false)
+                .setScopes(new String[] { "user-read-email"}) // <--- Change the scope of your requested token here
+                .setCampaign("your-campaign-token")
+                .build();
+    }
+    private Uri getRedirectUri() {
+        return Uri.parse(REDIRECT_URI);
+    }
+    private void setTextAsync(final String text, TextView textView) {
+        runOnUiThread(() -> textView.setText(text));
     }
 }

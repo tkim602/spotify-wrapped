835d7dd30e6faf39d6df83589eb587343cad5136
Taehoonryu04 <taehoonryu10@gmail.com>
2024-03-28 15:13:48 -0400

    gallery page

--- app/src/main/java/com/example/spotifywrapped/Interfaces/Personalization.java
+++ app/src/main/java/com/example/spotifywrapped/Interfaces/Personalization.java
@@ -1,5 +1,6 @@
 package com.example.spotifywrapped.Interfaces;
 
+import com.example.spotifywrapped.Models.SpotifyArtistResponse;
 import com.example.spotifywrapped.Models.SpotifyTrackResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -8,4 +9,8 @@ import retrofit2.http.Header;
 public interface Personalization {
     @GET("v1/me/top/tracks?limit=5")
     Call<SpotifyTrackResponse> getTopTracks(@Header("Authorization") String authToken);
+
+    @GET("v1/me/top/artists?limit=6")
+    Call<SpotifyArtistResponse> getTopArtists(@Header("Authorization") String authToken);
 }
+
